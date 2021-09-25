3017a44291e4ad9095d60c4a11ff1484bc7213da
shivampaiprojects <81945228+shivampaiprojects@users.noreply.github.com>
2021-09-25 22:23:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-function preload(){
+function preload() {
 
 }
-function setup(){
-    canvas = createCanvas(640,480);
-    canvas.position(110,250);
-    video = createCapture(VIDEO);
-    video.hide();
-    tint_color = '';
-}
-function draw(){
-    image(video,0,0,640,480);
-    tint(tint_color);
-}
-function take_snapshot(){
-    filename = window.prompt('file name');
-    save(filename + '.png');
-}
-function filterTint(){
-    tint_color = document.getElementById('color').value;
 
+function setup() {
+  canvas = createCanvas(640, 480);
+  canvas.position(150, 150);
+  video = createCapture(VIDEO);
+  video.hide();
+
+}
+
+function draw() {
+  image(video, 230, 150, 220, 200);
+
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+  circle(50, 50, 80);
+
+  circle(50, 430, 80);
+
+  circle(590, 50, 80);
+
+  circle(590, 430, 80);
+
+  fill(0, 128, 0);
+  stroke(0, 128, 0);
+  rect(90, 40, 460, 20);
+
+  rect(90, 420, 460, 20);
+
+  rect(40, 90, 20, 300);
+
+  rect(580, 90, 20, 300);
+}
+
+function take_snapshot(){    
+  save('student_name.png');
 }
